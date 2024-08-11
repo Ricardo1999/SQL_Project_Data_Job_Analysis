2ca0573c331414c7ec5b859416bcfeff4ba3f0d9
rdcmem <ric.costamoraes@gmail.com>
2024-08-11 19:47:33 -0300

    change to Data Scientist

--- project_sql/2_skills_required_for_top_paying_jobs.sql
+++ project_sql/2_skills_required_for_top_paying_jobs.sql
@@ -30,21 +30,16 @@ WITH
     FROM job_postings_fact
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE 
-        job_title_short = 'Data Analyst' AND
+        job_title_short = 'Data Scientist' AND
         job_location = 'Anywhere' AND
         salary_year_avg IS NOT NULL
     ORDER BY salary_year_avg DESC
     LIMIT 10)
 
 SELECT
-    top_payed_jobs.job_id,
     top_payed_jobs.job_title,
     skill_jobs.skills,
     top_payed_jobs.salary_year_avg
 
 FROM skill_jobs
 INNER JOIN top_payed_jobs ON skill_jobs.job_id = top_payed_jobs.job_id;
-
-
-
-
